4742fd405f771ad49f38b92c5b2c675fa39087f4
Adam Bezecny (DHL IT Services) <Adam.Bezecny@dhl.com>
2020-09-05 23:53:41 +0200

    AES-256-CBC encryption implemented

--- src/aes.rs
+++ src/aes.rs
@@ -1,23 +1,132 @@
-/// TBD: let's try this https://docs.rs/openssl/0.9.24/openssl/aes/index.html
-use crate::errors::Result;
-use aes::Aes256;
+use crate::errors::{Error, Result};
+use crypto::buffer::{BufferResult, ReadBuffer, WriteBuffer};
+use crypto::{aes, blockmodes, buffer};
+use hex;
+use rand::RngCore;
 
-fn encrypt(test: &str, encryption_key: &str) -> String {
-    "".to_owned()
+/// for implementation details see https://github.com/DaGenix/rust-crypto/blob/master/examples/symmetriccipher.rs#L17
+fn encrypt_impl(data: &[u8], key: &[u8], iv: &[u8]) -> Result<Vec<u8>> {
+    let mut encryptor =
+        aes::cbc_encryptor(aes::KeySize::KeySize256, key, iv, blockmodes::PkcsPadding);
+
+    let mut final_result = Vec::<u8>::new();
+    let mut read_buffer = buffer::RefReadBuffer::new(data);
+    let mut buffer = [0; 4096];
+    let mut write_buffer = buffer::RefWriteBuffer::new(&mut buffer);
+
+    loop {
+        let result = encryptor.encrypt(&mut read_buffer, &mut write_buffer, true)?;
+
+        final_result.extend(
+            write_buffer
+                .take_read_buffer()
+                .take_remaining()
+                .iter()
+                .map(|&i| i),
+        );
+
+        match result {
+            BufferResult::BufferUnderflow => break,
+            BufferResult::BufferOverflow => {}
+        }
+    }
+
+    Ok(final_result)
 }
 
-fn decrypt(test: &str, encryption_key: &str) -> String {
-    "".to_owned()
+/// for implementation details see https://github.com/DaGenix/rust-crypto/blob/master/examples/symmetriccipher.rs#L82
+fn decrypt_impl(encrypted_data: &[u8], key: &[u8], iv: &[u8]) -> Result<Vec<u8>> {
+    let mut decryptor =
+        aes::cbc_decryptor(aes::KeySize::KeySize256, key, iv, blockmodes::PkcsPadding);
+
+    let mut final_result = Vec::<u8>::new();
+    let mut read_buffer = buffer::RefReadBuffer::new(encrypted_data);
+    let mut buffer = [0; 4096];
+    let mut write_buffer = buffer::RefWriteBuffer::new(&mut buffer);
+
+    loop {
+        let result = decryptor.decrypt(&mut read_buffer, &mut write_buffer, true)?;
+        final_result.extend(
+            write_buffer
+                .take_read_buffer()
+                .take_remaining()
+                .iter()
+                .map(|&i| i),
+        );
+        match result {
+            BufferResult::BufferUnderflow => break,
+            BufferResult::BufferOverflow => {}
+        }
+    }
+
+    Ok(final_result)
+}
+
+pub fn encrypt(text: &str, encryption_key: &str) -> Result<String> {
+    let key = encryption_key.as_bytes();
+    let data_to_encrypt = text.as_bytes();
+    let mut iv: [u8; 16] = [0; 16];
+
+    let mut rng = rand::rngs::OsRng::default();
+    rng.fill_bytes(&mut iv);
+
+    let encrypted_data = encrypt_impl(data_to_encrypt, &key, &iv)?;
+    let strigified_data = hex::encode(encrypted_data);
+    let iv = hex::encode(&iv);
+    Ok(format!("{}:{}", iv, strigified_data))
+}
+
+pub fn decrypt(text: &str, encryption_key: &str) -> Result<String> {
+    let split: Vec<&str> = text.split(":").collect();
+    if split.len() != 2 {
+        return Err(Error::new(
+            "Wrong format of encrypted data, must be <<iv>>:<<data>>!".to_owned(),
+        ));
+    }
+    let iv = hex::decode(split[0])?;
+    let encrypted_text = hex::decode(split[1])?;
+
+    let decrytped_text = decrypt_impl(&encrypted_text[..], encryption_key.as_bytes(), &iv[..])?;
+    let stringified_data = String::from_utf8(decrytped_text)?;
+    Ok(stringified_data)
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
 
-    // cargo test -- --show-output test_encrypt
+    // cargo test -- --show-output test_encrypt_impl_decrypt_impl
     #[test]
-    fn test_encrypt() -> Result<()> {
-        let encrypted = encrypt("hello there", "tbd...");
+    fn test_encrypt_impl_decrypt_impl() -> Result<()> {
+        let message = "Hello World!";
+
+        let mut key: [u8; 32] = [0; 32];
+        let mut iv: [u8; 16] = [0; 16];
+
+        let mut rng = rand::rngs::OsRng::default();
+
+        rng.fill_bytes(&mut key);
+        rng.fill_bytes(&mut iv);
+
+        let encrypted_data = encrypt_impl(message.as_bytes(), &key, &iv).ok().unwrap();
+        let decrypted_data = decrypt_impl(&encrypted_data[..], &key, &iv).ok().unwrap();
+
+        assert_eq!(message.as_bytes(), &decrypted_data[..]);
+
+        Ok(())
+    }
+
+    // cargo test -- --show-output test_encrypt_decrypt
+    #[test]
+    fn test_encrypt_decrypt() -> Result<()> {
+        let data_to_encrypt = "Adam was here!";
+        let secret = "546191f3-ac70-43c3-b9ad-a26d8fds";
+
+        let encrypted = encrypt(data_to_encrypt, secret)?;
+        let decrypted = decrypt(&encrypted, secret)?;
+
+        assert_eq!(data_to_encrypt, decrypted);
+
         Ok(())
     }
 }
--- src/errors.rs
+++ src/errors.rs
@@ -1,3 +1,5 @@
+use crypto::symmetriccipher;
+use hex;
 use std::result;
 
 #[derive(Debug)]
@@ -15,6 +17,38 @@ pub type Result<T> = result::Result<T, Error>;
 
 impl From<jsonwebtoken::errors::Error> for Error {
     fn from(error: jsonwebtoken::errors::Error) -> Error {
+        Error {
+            message: format!("{}", error),
+        }
+    }
+}
+
+impl From<symmetriccipher::SymmetricCipherError> for Error {
+    fn from(error: symmetriccipher::SymmetricCipherError) -> Error {
+        Error {
+            message: format!("{:#?}", error),
+        }
+    }
+}
+
+impl From<std::string::FromUtf8Error> for Error {
+    fn from(error: std::string::FromUtf8Error) -> Error {
+        Error {
+            message: format!("{}", error),
+        }
+    }
+}
+
+impl From<std::str::Utf8Error> for Error {
+    fn from(error: std::str::Utf8Error) -> Error {
+        Error {
+            message: format!("{}", error),
+        }
+    }
+}
+
+impl From<hex::FromHexError> for Error {
+    fn from(error: hex::FromHexError) -> Error {
         Error {
             message: format!("{}", error),
         }
