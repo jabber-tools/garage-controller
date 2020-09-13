f89d8feff4148849233d53bd60f67067ae73837d
Adam Bezecny (DHL IT Services) <Adam.Bezecny@dhl.com>
2020-09-13 21:01:49 +0200

    simple startup script added

---
+++ examples/startme.sh
@@ -0,0 +1,13 @@
+#! /bin/sh
+
+#enable application logging up to debug level
+export RUST_LOG=debug
+
+# make sure default gateway is configured so that we have connectivity to internet
+# correctly we should first check:
+#   sudo route -n
+# but add route action is idempotent so it does not really matter
+sudo route add default gw 192.168.1.1
+
+#run the app!
+./garage-controller --config-file ./app_config.toml
